96c8b8da8d8838b1b769e6087e3143b8133c610a
Artia Aryatama <artyaaryatama21@gmail.com>
2021-10-29 00:13:20 +0800

    menambahkan struktur organisasi

--- views/pengurus.php
+++ views/pengurus.php
@@ -13,6 +13,9 @@
 
     <!-- font awesome -->
     <script src="https://kit.fontawesome.com/54218bd639.js" crossorigin="anonymous"></script>
+   
+    <!-- font awesome -->
+    <!-- <script src="../assets/chart/orgchart.js"></script> -->
   </head>
   <body>
 
@@ -56,7 +59,103 @@
     <!-- ==END OF HEADER== -->
 
     <!-- ==MAIN CONTENT== -->
-tulis disini kontennya
+    <div class="wrapper-pengurus">
+      <div class="jumbotron">
+        <div class="container">
+          <h3 class="display-4 text-center"> Struktur Organisasi </h3>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+          <ul class="tree">
+            <li class="mabigus"> 
+              <span>MABIGUS
+                <hr>
+                <b>Jane Doe</b>
+              </span>
+              <ul>
+                <li class="pembina"> 
+                  <span>PEMBINA UTAMA
+                    <hr>
+                    <b>Jane Doe</b>
+                  </span>
+                  <ul class="space-ketua">
+                    <li class="ketua"> 
+                      <span >KETUA DEWAN PUTRA
+                        <hr>
+                        <b>Jane Doe</b>
+                      </span>
+                      <ul>
+                        <li class="sekertaris">
+                          <span>SEKERTARIS PUTRA
+                            <hr>
+                            <b>Jane Doe</b>
+                          </span></li>
+                      </ul>
+                      <ul>
+                        <li class="bendhara">
+                          <span>BENDAHARA PUTRA
+                            <hr>
+                            <b>Jane Doe</b>
+                          </span></li>
+                      </ul>
+                      <ul>
+                        <li class="pemangku">
+                          <span>PEMANGKU ADAT PUTRA
+                            <hr>
+                            <b>Jane Doe</b>
+                          </span></li>
+                      </ul>
+                      <ul>
+                        <li class="anggota">
+                          <span><b>ANGGOTA</b>
+                          </span></li>
+                      </ul>
+                    </li>
+                    <li class="ketua"> 
+                      <span>KETUA DEWAN PUTRI
+                        <hr>
+                        <b>Jane Doe</b>
+                    </span>
+                    <ul>
+                      <li class="sekertaris">
+                        <span>SEKERTARIS PUTRI
+                          <hr>
+                          <b>Jane Doe</b>
+                        </span></li>
+                    </ul>
+                    <ul>
+                      <li class="bendhara">
+                        <span>BENDAHARA PUTRI
+                          <hr>
+                          <b>Jane Doe</b>
+                        </span></li>
+                    </ul>
+                    <ul>
+                      <l class="pemangku">
+                        <span>PEMANGKU ADAT PUTRI
+                          <hr>
+                          <b>Jane Doe</b>
+                        </span></li>
+                    </ul>
+                    <ul>
+                      <li class="anggota">
+                        <span><b>ANGGOTA</b>
+                        </span></li>
+                    </ul>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-3"></div>
+      </div>
+
+    </div>
     <!-- ==END OF MAIN CONTENT== -->
 
     <!-- ==FOOTER== -->
