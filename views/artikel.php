eab69410e2f35b63ee09117640f376be687473fa
Artia Aryatama <artyaaryatama21@gmail.com>
2021-10-29 18:39:19 +0800

    menambahkan style artikel'

--- views/artikel.php
+++ views/artikel.php
@@ -56,7 +56,101 @@
     <!-- ==END OF HEADER== -->
 
     <!-- ==MAIN CONTENT== -->
-    
+    <div class="wrapper-artikel mt-5">
+       <div class="container">
+         <div class="title">
+           <h1 class="mb-5">Sejarah Kemerdekaan Indonesia</h1>
+            <div class="detail-artikel">
+              <ul>
+                <li class="penulis">Reinhart</li>
+                <li class="pr-1 pl-1">|</li>
+                <li class="hari-publikasi">Jum'at</li>,   <!-- KOMA JGN DIHAPUS -->
+                <li class="tanggal-publish">10 Mei 2021</li>
+              </ul>
+            </div>
+         </div>
+        </div>
+        <div class="konten">
+         <div class="jumbotron">
+          <!-- foto jumbotron ada di css -->
+         </div>
+         <div class="container mt-5">
+          <p class="isi-konten">
+          The standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p class="isi-konten">
+          The standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p class="isi-konten">
+          The standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p class="isi-konten">
+          The standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+         </div>
+        </div>
+        <!-- <hr> -->
+
+        <div class="artikel-lainnya">
+          <div class="container">  
+            <ul>
+              <li><p>Artikel Lainnya</p></li>
+              <li><a href="previewArtikel.php">Lihat Artikel Lainnya <i class=" pl-1 far fa-arrow-alt-circle-right"></i></a></li>
+            </ul>
+            <hr>
+
+
+            <div class="card-artikel">
+              <div class="row">
+
+                <div class="col-md-4 col-sm-12 mb-3">
+                  <a href="artikel.php">
+                    <div class="card h-100">
+                      <div class="foto-card">
+                        <img src="../assets/image/header-jumbotron.jpg" class="card-img-top" alt="...">
+                      </div>
+                      <div class="card-body">
+                        <p>Sejarah Kemerdekaan Indonesia</p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+
+                <div class="col-md-4 col-sm-12 mb-3">
+                  <a href="artikel.php">
+                    <div class="card h-100">
+                        <div class="foto-card">
+                          <img src="../assets/image/logo.jpeg" class="card-img-top" alt="...">
+                        </div>
+                      <div class="card-body">
+                        <p>Sejarah Kemerdekaan  dasd asdas ad Kemerdekaan  dasd asdas addasdas</p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+
+                <div class="col-md-4 col-sm-12 mb-3">
+                  <a href="artikel.php">
+                    <div class="card h-100">
+                        <div class="foto-card">
+                          <img src="../assets/image/header-jumbotron.jpg" class="card-img-top" alt="...">
+                        </div>
+                      <div class="card-body">
+                        <p>Sejarah Kemerdekaan Indonesia</p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+
+              </div>
+            </div>
+
+
+
+
+          </div>
+        </div>
+    </div>
     <!-- ==END OF MAIN CONTENT== -->
 
     <!-- ==FOOTER== -->
