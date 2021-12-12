fc0f20ae6f094393bf20a1f79c534cab3cead8be
William Adam <williamadm8@gmail.com>
2021-12-12 21:57:16 +0800

    menambahkan login page

---
+++ views/login.php
@@ -0,0 +1,42 @@
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../assets/stylesheet/style.css" type="text/css">
+
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/54218bd639.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="wrapper-login">
+      <div class="login-title">
+        <div class="title-img">
+          <img src="../assets/image/login.png" alt="Scout group" />
+        </div>
+        <div class="title-text">
+          <h1>PARAGA</h1>
+          <h1>SCOUT</h1>
+        </div>
+        <div class="login-transparent-bg"></div>
+      </div>
+
+      <div class="login-info">
+        <h1>Login</h1>
+        <div class="login-input">
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Password" />
+        </div>
+        <a href="">Lupa password?</a>
+        <div class="button">
+          <button type="button" class="btn btn-warning">Login</button>
+        </div>
+      </div>
+    </div>
+</body>
+</html>
